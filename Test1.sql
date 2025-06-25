e3c64bf87e0f0ebdccc9ac875a5b4458005bf96a
rekls2311 <80893626+rekls2311@users.noreply.github.com>
2025-06-25 13:57:15 +1000

    Update Test1.sql

--- Test1.sql
+++ Test1.sql
@@ -57,3 +57,34 @@ INNER JOIN public.film_actor AS fa ON f.film_id = fa.film_id
 INNER JOIN public.actor AS a ON fa.actor_id = a.actor_id
 GROUP BY a.first_name, a.last_name
 ORDER BY COUNT(*) DESC
+
+--Question 6: Task: Tìm các địa chỉ không liên quan đến bất kỳ khách hàng nào.
+SELECT COUNT(c.address_id) 
+FROM public.address as c
+LEFT JOIN public.customer  as a
+ON c.address_id = a.address_id
+WHERE a.address_id IS NULL
+--Answer:4
+
+--question 7: Task: Danh sách các thành phố và doanh thu tương ừng trên từng thành phố. Question: Thành phố nào đạt doanh thu cao nhất ?
+SELECT city.city, SUM(payment.amount) AS "total_amount"
+FROM public.payment AS payment
+JOIN public.customer AS customer ON   payment.customer_id = customer.customer_id 
+JOIN public.address AS address ON customer.address_id = address.address_id
+JOIN public.city AS city ON address.city_id = city.city_id
+GROUP BY city.city
+ORDER BY SUM(payment.amount) desc
+--Answer: Cape Coral : 221.55
+
+--question 8: Task: Tạo danh sách trả ra 2 cột dữ liệu: Question: Thành phố của đất nước nào đat doanh thu thấp nhất ?
+SELECT CONCAT(country.country, ' , ', city.city) AS "Thanh Pho va Dat Nuoc", SUM(payment.amount) AS "total_amount"
+FROM public.payment AS payment
+JOIN public.customer AS customer ON   payment.customer_id = customer.customer_id 
+JOIN public.address AS address ON customer.address_id = address.address_id
+JOIN public.city AS city ON address.city_id = city.city_id
+JOIN public.country AS country ON city.country_id = country.country_id
+GROUP BY CONCAT(country.country, ' , ', city.city)
+ORDER BY SUM(payment.amount) asc
+--Answer: United States, Tallahassee : 50.85.
+
+
